be5ce83355f751ae4287d8d2feb9051e9efdb6c8
Wordbe <seonghojin3@gmail.com>
2022-08-21 00:35:34 +0900

    infra: multi-module setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,21 +23,17 @@ subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "kotlin-spring")
     apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "kotlin-jpa")
 
     dependencies {
         implementation("com.auth0:java-jwt:3.19.2")
         implementation("io.github.microutils:kotlin-logging:1.12.5")
-
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-        implementation("org.springframework.boot:spring-boot-starter-web")
 
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
         runtimeOnly("com.h2database:h2")
+
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 
