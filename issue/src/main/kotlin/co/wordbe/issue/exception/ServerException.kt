47bd709e02a5ce04eeb7941030e15aa9af7590d1
Wordbe <seonghojin3@gmail.com>
2022-09-14 17:44:05 +0900

    feat: sealed class 예시

--- issue/src/main/kotlin/co/wordbe/issue/exception/ServerException.kt
+++ issue/src/main/kotlin/co/wordbe/issue/exception/ServerException.kt
@@ -14,3 +14,16 @@ data class NotFoundException(
 data class UnauthorizedException(
     override val message: String = "인증 정보가 잘못되었습니다",
 ) : ServerException(401, message)
+
+// sealed class example
+//data class SomeNewException(
+//    override val message: String = "새로운 예외 입니다.",
+//) : ServerException(500, message)
+//
+//class ErrorManager {
+//    private val serverErrorMap = mutableMapOf<Int, ServerException>()
+//    fun add(serverException: ServerException) = when (serverException) { // compile error
+//        is NotFoundException -> serverErrorMap[serverException.code] = serverException
+//        is UnauthorizedException -> serverErrorMap[serverException.code] = serverException
+//    }
+//}
